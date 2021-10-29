7d97c453b44b6718e84a76c5ca4d8ea6b9dce20b
Shail Patel <38965795+shail1007@users.noreply.github.com>
2021-10-29 15:11:58 +1100

    Update 4_basicFunctions.py

--- Lesson 1 - Basics/4_basicFunctions.py
+++ Lesson 1 - Basics/4_basicFunctions.py
@@ -1,32 +1,36 @@
-#basic functions
-import cv2
-img = cv2.imread('..\Photos\cat.jpg')
-cv2.imshow('Cat', img)
 
-#convert to greyscale
+import cv2
+#Read img
+img = cv2.imread('Images/stock-photo.jpg')
+#cv2.imshow('Cat', img)
+
+#Greyscale function
 greyscale=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+greyscale=cv2.resize(greyscale,(500,500))
 cv2.imshow('Grey',greyscale)
 
-#blur
+#Blur function
 blur=cv2.GaussianBlur(img,(9,9),cv2.BORDER_DEFAULT)
 #ksize has to be odd numbers
+blur=cv2.resize(blur,(500,500))
 cv2.imshow('Blur',blur)
 
-#canny
+#Canny function
 canny=cv2.Canny(img,125,200)
+canny=cv2.resize(canny,(500,500))
 cv2.imshow('Canny',canny)
 
-#dilate
-#used to accentuate edge cases
+#Dilate
 dilated=cv2.dilate(canny,(3,3),iterations=1)
+dilated=cv2.resize(dilated,(500,500))
 cv2.imshow('Dilated',dilated)
 
-#erode
-#used to diminish edge cases
+#Erode function
 eroded=cv2.erode(dilated,(3,3),iterations=1)
+eroded=cv2.resize(eroded,(500,500))
 cv2.imshow('Eroded',eroded)
 
-#resize
+#Resize
 resized=cv2.resize(img,(500,500))
 cv2.imshow('Resize',resized)
 
